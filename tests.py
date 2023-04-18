2334c6589034c04988f309eefe49909ef31d1360
AlanFromJapan <alanfromjapan@gmail.com>
2023-04-18 23:07:40 +0900

    FIX renamed parameter ... but not in tests.py

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@ from PIL import Image
 
 img = grbl2image.processFile(os.path.join("sample.gcode", "Test gcode 2.nc"), color="blue")
 
-img = grbl2image.processFile(os.path.join("sample.gcode", "Test gcode 1.nc"), targetImage=img, color="red", voffset=300)
+img = grbl2image.processFile(os.path.join("sample.gcode", "Test gcode 1.nc"), targetImage=img, color="red", yoffset=300)
 
-#img = grbl2image.processFile("sample.gcode/Tokens 6 - Parnast final.nc", targetImage=img, color="black", voffset=600)
+#img = grbl2image.processFile("sample.gcode/Tokens 6 - Parnast final.nc", targetImage=img, color="black", yoffset=600)
 
 #final flip
 img = img.transpose(Image.FLIP_TOP_BOTTOM)

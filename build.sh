328a200ecc569c45e26232c207cb23cc0dc254b3
AlanFromJapan <alanfromjapan@gmail.com>
2024-04-17 18:14:02 +0900

    Cosmetic changes to the script to make it more ledgible

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+CYAN='\033[0;36m'
+
+
 echo "Clear old distribution files"
 rm dist/*
 
@@ -8,8 +13,13 @@ python3 -m build
 
 
 echo "Build done."
-echo "If everything went fine, do you want to push it to PyPi PROD?"
-echo "By the way, you remembered to update the version number before that right?"
-echo "Think twice, now is good time to Ctrl+C this..."
+echo -e "${CYAN}If everything went fine, do you want to push it to PyPi ${RED}PROD${CYAN}?"
+echo -e "By the way, you remembered to update the version number before that right?"
+echo -e "Think twice, now is good time to Ctrl+C this...$NC"
 read -n1 -s -r -p $'Press any key to continue...\n' key
+
+
+echo "You will be asked for a login password: it's not the pypi password, it's a token."
+echo -e "Username: ${RED}__token__${NC}"
+echo -e "Password: ${RED}Your token${NC}"
 python3 -m twine upload dist/*
